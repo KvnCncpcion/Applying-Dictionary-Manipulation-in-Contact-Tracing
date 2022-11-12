82e83c489a09f9c54ea6645f97f49ce14158df32
KvnCncpcion <kvncncpcion@gmail.com>
2022-11-12 22:07:14 +0800

    Declaring Empty Dictionary

--- Dictionary Manipulation.py
+++ Dictionary Manipulation.py
@@ -1,3 +1,6 @@
 print("********************PROGRAMMED BY***************************")
 print("*****************Kevin Joseph G. Concepcion*****************")
 print("******************BSCOE 2-2*********************************")
+
+# Declaring Empty Dictionary
+contact_info = {}
