bfbc71b12ce182b57b29dee61c28ba41e70a7a47
KvnCncpcion <kvncncpcion@gmail.com>
2022-11-12 22:10:46 +0800

    Adding the Menu

--- Dictionary Manipulation.py
+++ Dictionary Manipulation.py
@@ -4,3 +4,11 @@ print("******************BSCOE 2-2*********************************")
 
 # Declaring Empty Dictionary
 contact_info = {}
+
+# Adding of Menu
+print("")
+print("============== MENU ================")
+print("1 -> Add an Item")
+print("2 -> Search")
+print("3 -> Exit (y/n)")
+print("")
